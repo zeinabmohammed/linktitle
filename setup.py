c27f31ed20a4929756ce4ccbf26f4e5e705676a7
Zynap <43417377+zeinabmohammed@users.noreply.github.com>
2021-09-29 11:51:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
 import re, ast
 
 # get version from __version__ variable in linktitle/__init__.py
@@ -10,8 +10,8 @@ with open('linktitle/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
-
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 setup(
 	name='linktitle',
 	version=version,
